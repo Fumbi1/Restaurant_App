f1781ab5f372583df5e86aad8508d1932f82004e
Fumbi1 <olumuyiwafumbi@gmail.com>
2023-11-23 13:44:22 +0100

    Minor fixes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 
 export const metadata = {
-  title: 'Restaurant',
+  title: 'Nique Restaurant',
   description: 'We make the best meals and give the best services',
   name: 'viewport',
   content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0',
